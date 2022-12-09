be5382ec303354eea107cc2b67746895310403c7
Kacper Dabrowski <kacper76dab@gmail.com>
2022-12-09 16:39:30 +0000

    resrvation improvement and styling

--- scripts/reservations.js
+++ scripts/reservations.js
@@ -1,5 +1,6 @@
 // code by Kacper Dabrowski
 
+/** first functions triggered by onload of body */
 
 /**Function 
  * to set up starting date for input element of calendar (date)
@@ -15,22 +16,43 @@ function setDate(){
         const inputDate = document.getElementById("startDate");
         inputDate.value = isoDate;
         inputDate.min = isoDate;
+
+        /** Also to calculate end date and fill output element of form */
+        setEndDate();
 }
 
+/** function read starting date end number of days and calculate end date
+ * end put it in output #endDate element
+ */
+function setEndDate() {
+        const startDate = document.getElementById("startDate").value;
+        const days = parseInt(document.getElementById("days").value);
+        let endDate = calculateEndaDate(startDate,days);
+ 
+        // write result to output element
+        document.getElementById("endDate").value = endDate;
+}
+
+/** Global variables 
+ * that can be used in all functions 
+ * */
 let rooms = [];
-let roomKind = ["Suite","Single","Twin","Double"];
+const roomKind = ["Suite","Single","Twin","Double"];
+const maxAdults = [5,1,2,2];
+const maxKids = [5,1,2,2];
 let roomPrice = [150,50,80,100];
 
 function updateResInfo()
 {
         let room = document.getElementById("room").value;
         let startDate = document.getElementById("startDate").value;
-        let noOfDays = parseInt(document.getElementById("noOfDays").value);
-        let noOfPeople = parseInt(document.getElementById("noOfPeople").value);
+        let noOfDays = parseInt(document.getElementById("days").value);
+        let noOfPeople = parseInt(document.getElementById("adults").value);
+        let noOfKids = parseInt(document.getElementById("kids").value);
 
 
         if (noOfDays && noOfDays>0) {
-                let newBooking = [room,startDate,noOfDays,noOfPeople];
+                let newBooking = [room,startDate,noOfDays,noOfPeople,noOfKids];
                 rooms.push(newBooking);
                 showRooms();
         }
@@ -54,23 +76,35 @@ function showRooms()
         // header of table
         table += "<tr><th class='booking'>Room</th><th class='booking'>Arrival</th>";
         table += "<th class='booking'>last day</th><th class='booking'>Price</th>";
-        table += "<th class='booking'>People</th><th class='booking'>Remove</th></tr>";
+        table += "<th class='booking'>Adults</th><th class='booking'>Kids</th>";
+        table += "<th class='booking'>Remove</th></tr>";
+        
         // lines from array rooms[]
         for (i=0; i<rooms.length; i++){
-                table += "<tr>";
-                table += "<td class='booking'>"+roomKind[rooms[i][0]]+"</td>";
-                table += "<td class='booking'>"+rooms[i][1]+"</td>";
-                // calculate last day
-                const arrivalDate = new Date(rooms[i][1]);
-                let lastDay = arrivalDate;
-                let noOfDays = parseInt( rooms[i][2]);
-                let price = roomPrice[rooms[i][0]];
-                lastDay.setDate(arrivalDate.getDate()+noOfDays);
-                table += "<td class='booking'>"+lastDay.toISOString().slice(0,10)+"</td>";
-                table += "<td class='booking'>"+noOfDays*price+"</td>";
-                table += "<td class='booking'>"+rooms[i][3]+"</td>";
-                table += "<td class='booking'>"+"<button onclick='removeRoom("+i+");' class='form removeButton'>X</button>"+"</td>";
-                table += "</tr>";
+                // let tr = "<tr class='";
+                // if (i%2 == 0){
+                //         tr += "tr-even'>";
+                // }
+                // else {
+                //         tr += "tr-odd'>";
+                // }
+                if (rooms[i].length == 5) {
+                        table += "<tr class='booking'>";
+                        table += "<td>"+roomKind[rooms[i][0]]+"</td>";
+                        table += "<td>"+rooms[i][1]+"</td>";
+                        // calculate last day
+                        const arrivalDate = new Date(rooms[i][1]);
+                        let lastDay = arrivalDate;
+                        let noOfDays = parseInt( rooms[i][2]);
+                        let price = roomPrice[rooms[i][0]];
+                        lastDay.setDate(arrivalDate.getDate()+noOfDays);
+                        table += "<td>"+lastDay.toISOString().slice(0,10)+"</td>";
+                        table += "<td>"+noOfDays*price+"</td>";
+                        table += "<td>"+rooms[i][4]+"</td>";
+                        table += "<td>"+rooms[i][3]+"</td>";
+                        table += "<td>"+"<button onclick='removeRoom("+i+");' class='form removeButton'>X</button>"+"</td>";
+                        table += "</tr>";
+                }
         }
         table += "</table>"
         document.getElementById("showResInfo").innerHTML = info+table;
@@ -85,4 +119,29 @@ function showRoomPrice() {
         activeElement.style.display="none";
         activeElement = document.getElementById(roomElementsIds[room]);
         activeElement.style.display = "block";
+}
+
+/** function takes date as string and number of days
+ * end calculate new date after said number of days
+ * and returns it
+ */
+
+function calculateEndaDate(date,days){
+        const arrivalDate = new Date(date);
+        let lastDay = arrivalDate;      
+        lastDay.setDate(arrivalDate.getDate()+days);
+
+        return lastDay.toISOString().slice(0,10);
+}
+
+/** function changes maximum amount of people based on type of room  */
+function changeMaxPeople() {
+        // maxAdults and MaxKids are arrays that have maximum number of people for ech kind of room
+        // using same index as roomKind array
+        // defined in global variable section
+        let roomIndex = parseInt(document.getElementById("room").value);
+        
+        document.getElementById("adults").max = maxAdults[roomIndex];
+        document.getElementById("kids").max = maxKids[roomIndex];
+
 }
